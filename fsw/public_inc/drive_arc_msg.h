dd2bfc64c719aff82f872842fed91f2b2b761033
Tenzin Crouch <tenzinc@andrew.cmu.edu>
2021-10-19 12:54:48 -0400

    fixed formatting error

--- fsw/public_inc/drive_arc_msg.h
+++ fsw/public_inc/drive_arc_msg.h
@@ -1,17 +1,16 @@
 /****************************************************************
- * 
+ *
  * @file      drive_arc_msg.h
- * 
+ *
  * @brief     Type definition for Moonranger drive arc message
- * 
+ *
  * @version   1.0
  * @date      19 Oct 2021
- * 
+ *
  * @authors 	Tenzin Crouch
  * @author 		Carnegie Mellon University, Planetary Robotics Lab
- * 
+ *
  ****************************************************************/
-
 
 #ifndef _drive_arc_msg_h_
 #define _drive_arc_msg_h_
@@ -19,28 +18,25 @@
 #include "cfe_sb.h"
 #include "common_types.h"
 
-
 /*************************************************************************/
 /**
  * Type definition (MOONRANGER drive arc packet)
  */
-typedef struct
-{
-    uint64  timeStamp; // nanoseconds
-    float32 speed;     // m/s
-    float32 duration;  // seconds
-    float32 radius;    // meters
+typedef struct {
+  uint64 timeStamp; // nanoseconds
+  float32 speed;    // m/s
+  float32 duration; // seconds
+  float32 radius;   // meters
 
 } MOONRANGER_OptimalDriveArc_t;
 
 /**
  * Type definition (MOONRANGER drive arc telemetry)
  * @note includes CFS TLM Header with timestamp
-*/
+ */
 typedef struct {
-{
-    uint8 TlmHeader[CFE_SB_TLM_HDR_SIZE];
-    MOONRANGER_DriveArc_t data;
+  uint8 TlmHeader[CFE_SB_TLM_HDR_SIZE];
+  MOONRANGER_DriveArc_t data;
 } OS_PACK MOONRANGER_DriveArc_Tlm_t;
 
 // Message sizes
@@ -48,4 +44,3 @@ typedef struct {
 #define MOONRANGER_DRIVE_ARC_TLM_LNGTH sizeof(MOONRANGER_DriveArc_Tlm_t)
 
 #endif /* _drive_arc_msg_h_ */
-
