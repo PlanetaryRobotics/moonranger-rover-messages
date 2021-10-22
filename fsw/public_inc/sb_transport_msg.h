26249b5289144cf64835e88645bc0d578912c587
Kelvin Kang <kelvinkang@cmu.edu>
2021-10-21 21:05:23 -0400

    [SB Transport] update comments to explain better

--- fsw/public_inc/sb_transport_msg.h
+++ fsw/public_inc/sb_transport_msg.h
@@ -2,7 +2,10 @@
  *
  * @file      sb_transport_msg.h
  *
- * @brief     The software bus msg definition file
+ * @brief     The software bus large data transport msg definition file.
+ *            This msg and the API defined in sb_transport_lib is to be used
+ *            when you want to send a msg over SB that exceeds the size limit
+ *            of cFS' SB (65536 bytes).
  *
  * @version   1.0
  * @date      10/21/2021
@@ -20,6 +23,10 @@
 
 /**
  * @brief Generic Msg type for Software Bus Transport Library.
+ *
+ * WARNING: do not use this msg directly, it is recommended to use
+ * the API of sb_transport_lib to read/write the data as it provides
+ * mutex locks to prevent data race conditions.
  */
 typedef struct {
     CFE_SB_Msg_t MsgHdr;   // cFE generic msg header.
