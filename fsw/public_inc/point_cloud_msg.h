1f1f3f3345c40fa77b0f1aaedc20ab258d7a9ca6
Ankur Bodhe <abodhe@andrew.cmu.edu>
2021-11-10 15:14:48 -0500

    updated pointCloudMessage

--- fsw/public_inc/point_cloud_msg.h
+++ fsw/public_inc/point_cloud_msg.h
@@ -5,9 +5,9 @@
  * @brief     Type definition for Moonranger point cloud message
  * 
  * @version   1.0
- * @date      19 Oct 2021
+ * @date      4 Nov 2021
  * 
- * @authors 	Tenzin Crouch
+ * @authors 	Tenzin Crouch, Ankur Bodhe
  * @author 		Carnegie Mellon University, Planetary Robotics Lab
  * 
  ****************************************************************/
@@ -16,14 +16,38 @@
 #ifndef _point_cloud_msg_h_
 #define _point_cloud_msg_h_
 
-
+#include "cfe_sb.h"
+#include "common_types.h"
 /*************************************************************************/
 /*
 ** Type definition (MOONRANGER Point cloud)
 ** TODO: define actual structure
 */
 
-typedef MOONRANGER_NoArgsCmd_t MOONRANGER_Pcl_t;
+#define IMG_WIDTH  644
+#define IMG_HEIGHT 366
+
+typedef struct point_cloud_msg
+{
+    
+    CFE_TIME_SysTime_t  timestamp;
+    string  cameraLabel;
+    float32 pointCloud[IMG_WIDTH][IMG_HEIGHT][3];
+
+}MOONRANGER_Pcl_t;
+
+/**
+ * Type definition (MOONRANGER pointCloud telemetry)
+ * @note includes CFS TLM Header with timestamp
+ */
+typedef struct {
+  uint8 TlmHeader[CFE_SB_TLM_HDR_SIZE];
+  MOONRANGER_Pcl_t pcl_data;
+} OS_PACK MOONRANGER_Pcl_Tlm_t;
+
+// Message sizes
+#define MOONRANGER_POSE_LNGTH sizeof(MOONRANGER_Pcl_t)
+#define MOONRANGER_POSE_TLM_LNGTH sizeof(MOONRANGER_Pcl_Tlm_t)
 
 #endif /* _point_cloud_msg_h_ */
 
