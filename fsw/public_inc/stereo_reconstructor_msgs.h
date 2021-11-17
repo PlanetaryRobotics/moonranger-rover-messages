940dcbc719bcabd6ae5cc5053d1822f421d3e9a2
amyxlase <amyclin9@gmail.com>
2021-11-17 15:57:37 -0500

    add MIDs and message typedef

--- fsw/public_inc/stereo_reconstructor_msgs.h
+++ fsw/public_inc/stereo_reconstructor_msgs.h
@@ -1,22 +1,42 @@
-/**************************************************************** * * @file
- * stereo_reconstructor_msgs.h * * @brief stereo reconstructor msgs * *
- * @version 1.0 * @date 11/13 * * @authors Amy Lin * @author Carnegie Mellon
- * University, Planetary Robotics Lab * * * @note This file only contains app
+/**************************************************************** * * 
+ * @file stereo_reconstructor_msgs.h
+ * @brief stereo reconstructor msgs
+ * @version 1.0 * 
+ * @date 11/13 * * 
+ * @authors Amy Lin * 
+ * @author Carnegie Mellon University, Planetary Robotics Lab * * * 
+ * @note This file only contains app
  * specific command and * telemetry message definitions and command codes.
  * ****************************************************************/
-#ifndef _stereo_reconstructor_msgs_h #define _stereo_reconstructor_msgs_h
+#ifndef _stereo_reconstructor_msgs_h 
+#define _stereo_reconstructor_msgs_h
+
+/*************************************************************************/
 /*** STEREO App command codes*/
-#define STEREO_NOOP_CC 0 #define STEREO_RESET_COUNTERS_CC 1
-    /*************************************************************************/
-    /*** Type definition (generic "no arguments" command)*/ typedef struct {
+#define STEREO_NOOP_CC 0 
+#define STEREO_RESET_COUNTERS_CC 1
+
+/*************************************************************************/
+/*** Msg IDs*/
+
+#define STEREO_CMD_MID 0x1882
+#define STEREO_SEND_HK_MID 0x1883
+#define STEREO_HK_TLM_MID 0x0883
+
+/*************************************************************************/
+/*** Type definition (generic "no arguments" command)*/ 
+
+typedef struct {
     uint8 CmdHeader[CFE_SB_CMD_HDR_SIZE];
 } STEREO_NoArgsCmd_t;
+
 /*** The following commands all share the "NoArgs" format**** They are each
  * given their own type name matching the command name, which_open_mode** allows
  * them to change independently in the future without changing the prototype**
  * of the handler function*/
 typedef STEREO_NoArgsCmd_t STEREO_Noop_t;
 typedef STEREO_NoArgsCmd_t STEREO_ResetCounters_t;
+typedef STEREO_NoArgsCmd_t STEREO_Process_t;
 /*************************************************************************/ /***
                                                                                Type definition (STEREO App housekeeping)*/
 typedef struct {
