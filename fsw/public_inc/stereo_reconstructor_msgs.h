dd502cbcca76f95217b1573e35c6eb340553b94d
William Lee-Moore <wleemoor@andrew.cmu.edu>
2021-11-20 18:13:37 -0500

    Reserve unused message IDs and change to using those message IDs.

--- fsw/public_inc/stereo_reconstructor_msgs.h
+++ fsw/public_inc/stereo_reconstructor_msgs.h
@@ -1,30 +1,30 @@
-/**************************************************************** * * 
+/**************************************************************** * *
  * @file stereo_reconstructor_msgs.h
  * @brief stereo reconstructor msgs
- * @version 1.0 * 
- * @date 11/13 * * 
- * @authors Amy Lin * 
- * @author Carnegie Mellon University, Planetary Robotics Lab * * * 
+ * @version 1.0 *
+ * @date 11/13 * *
+ * @authors Amy Lin *
+ * @author Carnegie Mellon University, Planetary Robotics Lab * * *
  * @note This file only contains app
  * specific command and * telemetry message definitions and command codes.
  * ****************************************************************/
-#ifndef _stereo_reconstructor_msgs_h 
+#ifndef _stereo_reconstructor_msgs_h
 #define _stereo_reconstructor_msgs_h
 
 /*************************************************************************/
 /*** STEREO App command codes*/
-#define STEREO_NOOP_CC 0 
+#define STEREO_NOOP_CC 0
 #define STEREO_RESET_COUNTERS_CC 1
 
 /*************************************************************************/
 /*** Msg IDs*/
 
-#define STEREO_CMD_MID 0x1882
-#define STEREO_SEND_HK_MID 0x1883
-#define STEREO_HK_TLM_MID 0x0883
+#define STEREO_CMD_MID 0x18A0
+#define STEREO_SEND_HK_MID 0x18A1
+#define STEREO_HK_TLM_MID 0x18A2
 
 /*************************************************************************/
-/*** Type definition (generic "no arguments" command)*/ 
+/*** Type definition (generic "no arguments" command)*/
 
 typedef struct {
     uint8 CmdHeader[CFE_SB_CMD_HDR_SIZE];
