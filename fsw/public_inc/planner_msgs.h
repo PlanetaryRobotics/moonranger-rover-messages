8d07573f272a85718cf7c5c6be64ab104ba223f2
Ankur Bodhe <abodhe@andrew.cmu.edu>
2021-11-29 17:06:58 -0500

    updated planner_msgs.h with counters for pose,mesh,goal in PLANNER_HkTlm_Payload_t

--- fsw/public_inc/planner_msgs.h
+++ fsw/public_inc/planner_msgs.h
@@ -13,21 +13,6 @@
  * @note		This file only contains app specific command and
  * 				telemetry message definitions and command codes.
  ****************************************************************/
-
-/*********************************************************************
- * INSTRUCTIONS
- *
- * This file should contain all the type definitions for app specific messages
- *such as housekeeping / telemetry.
- *
- * It should also contain definitions for all command messages and command
- *codes.
- *
- * All message IDs should be placed in moonranger_messageids.h in the
- *appropriately marked location
- *
- * DELETE THIS BLOCK ONCE COMPLETE
- *******************************************************************/
 #ifndef __planner_msgs_h
 #define _planner_msgs_h
 
