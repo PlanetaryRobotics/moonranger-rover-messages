00073c71a58ffee0fdfe4b7429df83a1dba42cf0
Michael <89793126+mlang25@users.noreply.github.com>
2021-12-04 18:36:25 -0500

    added motor-msp reply msgs

--- fsw/public_inc/motor_controller_msgs.h
+++ fsw/public_inc/motor_controller_msgs.h
@@ -1,13 +1,13 @@
 /****************************************************************
  * 
- * @file      TODO
+ * @file      motor_controller_msgs.h
  * 
- * @brief     TODO
+ * @brief     messages send by motor controller
  * 
- * @version   TODO
- * @date   		TODO
+ * @version   1.0
+ * @date   		12/4/2021
  * 
- * @authors 	TODO
+ * @authors 	Michael Lang
  * @author 		Carnegie Mellon University, Planetary Robotics Lab
  * 
  * @note		This file only contains app specific command and 
@@ -15,23 +15,25 @@
  * 
  ****************************************************************/
 
-
-/*********************************************************************
- * INSTRUCTIONS
- * 
- * This file should contain all the type definitions for app specific messages such as
- * housekeeping / telemetry.
- * 
- * It should also contain definitions for all command messages and command codes.
- * 
- * All message IDs should be placed in moonranger_messageids.h in the appropriately marked location
- * 
- * DELETE THIS BLOCK ONCE COMPLETE
- *******************************************************************/
-
 #ifndef _motor_controller_msgs_h
 #define _motor_controller_msgs_h
 
+typedef struct {
+    volatile int32_t motor_hall_sensor;        // the ticks from hall sensor
+    volatile int32_t motor_desired_velocity;   // The commanded velocity
+    volatile int32_t motor_actual_velocity;    // The actual velocity
+    volatile int32_t motor_pwm;                // The motor pwm duty cycle
+    volatile int32_t motor_current;            // The current drawn by the motor
+    volatile int32_t motor_p_gain;               // The P gain of the motor scaled
+    volatile int32_t motor_i_gain;               // The I gain of the motor scaled
+    volatile int32_t motor_d_gain;               // The D gain of the motor scaled
+    volatile uint8_t motor_brake;              // The brake status of the motor
+    uint8_t _padding[3];
+} motor_health_msg_t;
+
+typedef struct{
+    motor_health_msg_t motors[5];
+}motor_health_msg_all_t;
 
 
 
