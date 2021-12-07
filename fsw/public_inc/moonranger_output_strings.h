1b597f7fddc6854c150590819f370e966b0a1f02
Ben Kolligs <benkolligs@gmail.com>
2021-12-07 15:18:25 -0500

    Added additional macros to indicate what information is beiing presented
    Verified the names of these strings do not clash
    
    Added MOON prefix to strings

--- fsw/public_inc/moonranger_output_strings.h
+++ fsw/public_inc/moonranger_output_strings.h
@@ -17,36 +17,41 @@
 #define ESCAPE    "\033["
 #define TERMINATE "m"
 /* Reset color back to normal with ANSI color codes */
-#define RESET "0"    TERMINATE
-#define END   ESCAPE RESET
+#define RESET_COLOR "0"    TERMINATE
+#define END_COLOR   ESCAPE RESET_COLOR
 
 /* Define ANSI color codes for strings */
-#define RED            "31" TERMINATE
-#define RED_BOLD       "1;" RED
-#define GREEN          "32" TERMINATE
-#define GREEN_BOLD     "1;" GREEN
-#define YELLOW         "33" TERMINATE
-#define YELLOW_BOLD    "1;" YELLOW
-#define BLUE           "34" TERMINATE
-#define BLUE_BOLD      "1;" BLUE
-#define MAGENTA        "35" TERMINATE
-#define MAGENTA_BOLD   "1;" MAGENTA
-#define CYAN           "36" TERMINATE
-#define CYAN_BOLD      "1;" CYAN
+#define COLOR_RED            "31" TERMINATE
+#define COLOR_RED_BOLD       "1;" COLOR_RED
+#define COLOR_GREEN          "32" TERMINATE
+#define GREEN_BOLD           "1;" COLOR_GREEN
+#define COLOR_YELLOW         "33" TERMINATE
+#define YELLOW_BOLD          "1;" COLOR_YELLOW
+#define COLOR_BLUE           "34" TERMINATE
+#define BLUE_BOLD            "1;" COLOR_BLUE
+#define COLOR_MAGENTA        "35" TERMINATE
+#define MAGENTA_BOLD         "1;" COLOR_MAGENTA
+#define COLOR_CYAN           "36" TERMINATE
+#define CYAN_BOLD            "1;" COLOR_CYAN
 
 
 /* Define macros to process different color strings */
-#define RED_STRING(string)          ESCAPE RED          string END
-#define RED_BOLD_STRING(string)     ESCAPE RED_BOLD     string END
-#define GREEN_STRING(string)        ESCAPE GREEN        string END
-#define GREEN_BOLD_STRING(string)   ESCAPE GREEN_BOLD   string END
-#define YELLOW_STRING(string)       ESCAPE YELLOW       string END
-#define YELLOW_BOLD_STRING(string)  ESCAPE YELLOW_BOLD  string END
-#define BLUE_STRING(string)         ESCAPE BLUE         string END
-#define BLUE_BOLD_STRING(string)    ESCAPE BLUE_BOLD    string END
-#define MAGENTA_STRING(string)      ESCAPE MAGENTA      string END
-#define MAGENTA_BOLD_STRING(string) ESCAPE MAGENTA_BOLD string END
-#define CYAN_STRING(string)         ESCAPE CYAN         string END
-#define CYAN_BOLD_STRING(string)    ESCAPE CYAN_BOLD    string END
+#define RED_STRING(string)          ESCAPE COLOR_RED          string END_COLOR
+#define RED_BOLD_STRING(string)     ESCAPE COLOR_RED_BOLD     string END_COLOR
+#define GREEN_STRING(string)        ESCAPE COLOR_GREEN        string END_COLOR
+#define GREEN_BOLD_STRING(string)   ESCAPE GREEN_BOLD         string END_COLOR
+#define YELLOW_STRING(string)       ESCAPE COLOR_YELLOW       string END_COLOR
+#define YELLOW_BOLD_STRING(string)  ESCAPE YELLOW_BOLD        string END_COLOR
+#define BLUE_STRING(string)         ESCAPE COLOR_BLUE         string END_COLOR
+#define BLUE_BOLD_STRING(string)    ESCAPE BLUE_BOLD          string END_COLOR
+#define MAGENTA_STRING(string)      ESCAPE COLOR_MAGENTA      string END_COLOR
+#define MAGENTA_BOLD_STRING(string) ESCAPE MAGENTA_BOLD       string END_COLOR
+#define CYAN_STRING(string)         ESCAPE COLOR_CYAN         string END_COLOR
+#define CYAN_BOLD_STRING(string)    ESCAPE CYAN_BOLD          string END_COLOR
+
+/* Define macros to represent the type of information we are presenting */
+#define MOON_ERROR_INFO(string)   RED_STRING(string)
+#define MOON_WARNING_INFO(string) YELLOW_STRING(string)
+#define MOON_SUCCESS_INFO(string) GREEN_STRING(string)
 
 #endif //_moonranger_output_strings_h_ header
