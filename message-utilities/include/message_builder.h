2133d9a460981f73892c75a85709818f76cff532
Tejas Rai <tejasrai25@gmail.com>
2022-01-09 02:02:01 -0500

    linting and union rename

--- message-utilities/include/message_builder.h
+++ message-utilities/include/message_builder.h
@@ -1,26 +1,26 @@
 #ifndef _message_builder_h
 #define _message_builder_h
-#include "common_types.h"
-#include "cfe_sb.h"
 #include "ccsds.h"
+#include "cfe_sb.h"
+#include "common_types.h"
+#include "goal_msg.h"
 #include "moonranger_msgids.h"
 #include "pose_msg.h"
-#include "wheel_velocity_command_msg.h"
 #include "teleop_msg.h"
-#include "goal_msg.h"
+#include "wheel_velocity_command_msg.h"
 
 #define SUCCESS 1
 #define FAILURE 0
 
 /**
- * 
+ *
  * A message consists of header + data
  * addressof(msg_buf_ptr) points to start of message (useful for receiving)
- * addressof(data_buf_ptr) points to start of data for all messages (useful for building)
- * 
+ * addressof(data_buf_ptr) points to start of data for all messages (useful for
+ * building)
+ *
  * */
 typedef union {
-
     uint8_t msg_buf_ptr;
 
     struct {
@@ -33,12 +33,14 @@ typedef union {
     MOONRANGER_Teleop_Cmd_t Teleop_Cmd;
     MOONRANGER_Goal_Tlm_t Goal_Tlm;
 
-} message_builder_u;
+} MOONRANGER_Message_u;
 
-int messageExtract(void *MsgPtr,int msg_len_bytes, message_builder_u* msg_container);
+int messageExtract(void* MsgPtr, int msg_len_bytes,
+                   MOONRANGER_Message_u* msg_container);
 
-int messageBuild(void* dataPtr,message_builder_u* msg_container,int data_len_bytes, int32 msgId);
+int messageBuild(void* dataPtr, MOONRANGER_Message_u* msg_container,
+                 int data_len_bytes, int32 msgId);
 
-CFE_SB_MsgId_t GetMsgId(void *MsgPtr);
+CFE_SB_MsgId_t GetMsgId(void* MsgPtr);
 
 #endif
--- message-utilities/src/utilities.c
+++ message-utilities/src/utilities.c
@@ -1,6 +1,6 @@
 #include "message_builder.h"
 
-CFE_SB_MsgId_t GetMsgId(void *ptr){
-  CFE_SB_Msg_t *MsgPtr = ptr;
-  return CFE_SB_GetMsgId(MsgPtr);
+CFE_SB_MsgId_t GetMsgId(void *ptr) {
+    CFE_SB_Msg_t *MsgPtr = ptr;
+    return CFE_SB_GetMsgId(MsgPtr);
 }
