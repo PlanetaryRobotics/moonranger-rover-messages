3ec1f402dfaaef9754d54043792e71bb288acb57
Tejas Rai <tejasrai25@gmail.com>
2022-01-10 15:43:36 -0500

    switched name back to message_builder_u

--- message-utilities/include/message_builder.h
+++ message-utilities/include/message_builder.h
@@ -33,12 +33,12 @@ typedef union {
     MOONRANGER_Teleop_Cmd_t Teleop_Cmd;
     MOONRANGER_Goal_Tlm_t Goal_Tlm;
 
-} MOONRANGER_Message_u;
+} message_builder_u;
 
 int messageExtract(void* MsgPtr, int msg_len_bytes,
-                   MOONRANGER_Message_u* msg_container);
+                   message_builder_u* msg_container);
 
-int messageBuild(void* dataPtr, MOONRANGER_Message_u* msg_container,
+int messageBuild(void* dataPtr, message_builder_u* msg_container,
                  int data_len_bytes, int32 msgId);
 
 CFE_SB_MsgId_t GetMsgId(void* MsgPtr);
--- message-utilities/src/message_builder.c
+++ message-utilities/src/message_builder.c
@@ -22,31 +22,39 @@
 #include <string.h>
 #include <sys/types.h>
 
-int messageExtract(void* MsgPtr, int msg_len_bytes,
-                   MOONRANGER_Message_u* msg_container) {
-    // Sanity check what we are about to do
-    if (msg_len_bytes <= sizeof(MOONRANGER_Message_u)) {
-        memcpy(&msg_container->msg_buf_ptr, MsgPtr, msg_len_bytes);
-    } else
-        return FAILURE;
 
-    return SUCCESS;
+int messageExtract(void *MsgPtr,int msg_len_bytes, message_builder_u* msg_container)
+{
+  // Sanity check what we are about to do
+  if(msg_len_bytes<=sizeof(message_builder_u))
+  {
+    memcpy(&msg_container->msg_buf_ptr,MsgPtr,msg_len_bytes);
+  }
+  else
+    return FAILURE;
+
+  return SUCCESS;
 }
 
-int messageBuild(void* dataPtr, MOONRANGER_Message_u* msg_container,
-                 int data_len_bytes, int32 msgId) {
-    int header_length = CFE_SB_TLM_HDR_SIZE;
-    // Fill the header
-    CFE_SB_InitMsg(&msg_container->msg_buf_ptr, (CFE_SB_MsgId_t)msgId,
-                   header_length + data_len_bytes, true);
 
-    // Fill the data
-    // Sanity check what we are about to do
-    if (data_len_bytes + header_length <= sizeof(MOONRANGER_Message_u)) {
-        memcpy(&msg_container->data_buf_ptr, dataPtr, data_len_bytes);
-    } else
-        return FAILURE;
+int messageBuild(void* dataPtr, message_builder_u* msg_container,int data_len_bytes, int32 msgId)
+{
 
-    // Size of built message
-    return header_length + data_len_bytes;
+  int header_length = CFE_SB_TLM_HDR_SIZE;
+  // Fill the header
+  CFE_SB_InitMsg(&msg_container->msg_buf_ptr, (CFE_SB_MsgId_t)msgId, header_length+ data_len_bytes, true);
+
+  // Fill the data
+  // Sanity check what we are about to do
+  if(data_len_bytes+header_length<=sizeof(message_builder_u))
+  {
+    memcpy(&msg_container->data_buf_ptr,dataPtr,data_len_bytes);
+  }
+  else
+    return FAILURE;
+
+  // Size of built message
+  return header_length+ data_len_bytes;
+
 }
+
