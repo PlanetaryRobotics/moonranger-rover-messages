a895eed4b749ea51a3bac7b71a080e8a0c630e3a
KJ Newman <kjkylenewman@gmail.com>
2022-02-06 15:27:34 -0500

    added nss structs

--- fsw/public_inc/nss_if_msp_msgs.h
+++ fsw/public_inc/nss_if_msp_msgs.h
@@ -1,13 +1,13 @@
 /****************************************************************
  * 
- * @file      TODO
+ * @file      nss_if_msp_msgs.h
  * 
- * @brief     TODO
+ * @brief     messages between master msp (E3) and nss msp (E6)
  * 
- * @version   TODO
- * @date   		TODO
+ * @version   1.0
+ * @date   		2/6/2021
  * 
- * @authors 	TODO
+ * @authors 	KJ Newman
  * @author 		Carnegie Mellon University, Planetary Robotics Lab
  * 
  * @note		This file only contains app specific command and 
@@ -15,24 +15,22 @@
  * 
  ****************************************************************/
 
-
-/*********************************************************************
- * INSTRUCTIONS
- * 
- * This file should contain all the type definitions for app specific messages such as
- * housekeeping / telemetry.
- * 
- * It should also contain definitions for all command messages and command codes.
- * 
- * All message IDs should be placed in moonranger_messageids.h in the appropriately marked location
- * 
- * DELETE THIS BLOCK ONCE COMPLETE
- *******************************************************************/
-
 #ifndef _nss_if_msp_msgs_h
 #define _nss_if_msp_msgs_h
 
+typedef struct {
+	volatile int8_t command1; // first command byte
+	volatile int8_t command2; // second command byte
+	uint8_t _padding[2];
 
+} set_nss_params_t
+
+typedef struct {
+	volatile int8_t telemetry[89]; // nss telemetry data
+	uint8_t reboot_counter; // median reboot counter from FRAM
+	uint8_t message_counter; // message counter incremented each time this is sent
+	uint8_t _padding;
+} nss_health_msg_t
 
 
 #endif /* _nss_if_msp_msgs_h */
