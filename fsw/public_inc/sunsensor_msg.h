de3a687f416afc451ddc0daeb614565da2c89030
Saksham Khurana <8430313+sakshamkhurana@users.noreply.github.com>
2022-02-09 13:59:08 -0800

    Factored appdata out

--- fsw/public_inc/sunsensor_msg.h
+++ fsw/public_inc/sunsensor_msg.h
@@ -19,28 +19,30 @@
 #include "moonranger_common_types.h"
 
 #define SYNC_WORD_LEN 4
-#define APP_DATA_LEN 9
-#define PHOTO_VOLT_DATA_LEN 16
+#define SYNC_WORD 0x1ACFFC1D // for use while sending messages
 
 /*************************************************************************/
 /**
  * @struct SunSensorCmd_t
  * @brief  Type definition for Sun sensor command
- * 
+ * @TODO: Should the sync_word be included in the message structure or 
+ *        should there be a separate base message without sync word for
+ *        use by cFS and another one with sync word for use by MSP?
  * */
 typedef struct {
     unsigned char sync_word[SYNC_WORD_LEN];  // sync word to indicate start of new frame, 0x1ACFFC1D
     unsigned char cmd_code;   // command code in 0xXX format
-                            // allowed values: 
-                            // 0x01 - request for voltages values of photocells without filtering
-                            // 0x03 - request for voltages values of photocells with filtering
-                            // 0x04 - request for angular positions (alpha, beta) and error code
-    unsigned char msg_len;    // length of commange message, always 0x1
+                              // allowed values: 
+                              // 0x01 - request for voltages values of photocells without filtering
+                              // 0x03 - request for voltages values of photocells with filtering
+                              // 0x04 - request for angular positions (alpha, beta) and error code
+    unsigned char msg_len;    // length of command message, always 0x1
     unsigned char checksum;   // dependent on command code, see struct description
-                            // allowed values: 
-                            // 0x02 - when command code 0x01
-                            // 0x04 - when command code 0x03
-                            // 0x05 - when command code 0x04
+                              // allowed values: 
+                              // 0x02 - when command code 0x01
+                              // 0x04 - when command code 0x03
+                              // 0x05 - when command code 0x04
+    
 } SunSensorCmd_t;
 
 /**
@@ -55,15 +57,44 @@ typedef struct {
 #define SUNSENSOR_CMD_LNGTH sizeof(SunSensorCmd_Cmd_t)
 
 /**
+ * @struct  voltage_data
+ * @brief   Type definition for Sun sensor voltage data field
+ * */
+typedef struct {
+    unsigned char uSSA1F[4];  // voltages are reported as 4 chunks of 4 bytes each
+    unsigned char uSSA2F[4];  // voltages are reported as 4 chunks of 4 bytes each
+    unsigned char uSSA3F[4];  // voltages are reported as 4 chunks of 4 bytes each
+    unsigned char uSSA4F[4];  // voltages are reported as 4 chunks of 4 bytes each
+    
+} voltage_data_t;
+
+/**
+ * @struct  app_data
+ * @brief   Type definition for Sun sensor voltage data field
+ * */
+typedef struct {
+    unsigned char alpha[4];     // alpha angle
+    unsigned char beta[4];      // beta angle
+    unsigned char err_code;  // error code
+                             // 0 No error. Angles were calculated successfully
+                             // 11 Albedo: Earth; Sun sensor does not see the Sun, but Earth, and 
+                             // the reflected sun-light is affecting measurement of the sensor.
+                             // 12 Albedo: Earth + Sun; Sun sensor sees the Sun and the Earth, 
+                             // because received solar radiation level is higher than 1360 
+                             // W/m2, with a tolerance of 10%, so a reflected sun-light is 
+                             // affecting measurement.
+                             // Other values: internal mfg error codes
+    
+} app_data_t;
+
+/**
  * @union  sun_sensor_data_t
  * @brief  Type definition for Sun sensor core data field
  * @TODO:  get this reviewed if there is a better way to do this
  * */
 typedef union {
-    unsigned char voltage_data[PHOTO_VOLT_DATA_LEN];  // magic number 16 comes from data sheet where
-                                           // voltages are reported as 4 chunks of 4 bytes each
-    unsigned char app_data[APP_DATA_LEN];  // magic number 9 comes from data sheet where alpha
-                                           // and beta angles are 4 bytes each, error code is 1 byte
+    voltage_data_t voltage_data;    // voltages are reported as 4 chunks of 4 bytes each
+    app_data_t app_data;            // sun angles reported with error code
 }  sun_sensor_data_t;
 
 /**
