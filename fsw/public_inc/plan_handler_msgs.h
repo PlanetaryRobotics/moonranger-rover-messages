00bb93bd1aa0c6e2c0c7b60160f14105790ae491
Srinivasan Vijayarangan <srinivasan@cmu.edu>
2022-07-01 20:57:21 +0000

    Added a new message ID and removed some unused typedefs

--- fsw/public_inc/plan_handler_msgs.h
+++ fsw/public_inc/plan_handler_msgs.h
@@ -24,45 +24,24 @@
 /*
 ** Plan Handler command codes
 */
-#define PLAN_HANDLER_NOOP_CC           0
-#define PLAN_HANDLER_RESET_COUNTERS_CC 1
-#define PLAN_HANDLER_PROCESS_CC        2
-
-/*************************************************************************/
-
-/*
-** Type definition (generic "no arguments" command)
-*/
-typedef struct
-{
-    uint8 CmdHeader[CFE_SB_CMD_HDR_SIZE];
-} PLAN_HANDLER_NoArgsCmd_t;
-
-/*
-** The following commands all share the "NoArgs" format
-**
-** They are each given their own type name matching the command name, which
-** allows them to change independently in the future without changing the prototype
-** of the handler function
-*/
-typedef PLAN_HANDLER_NoArgsCmd_t PLAN_HANDLER_NoopCmd_t;
-typedef PLAN_HANDLER_NoArgsCmd_t PLAN_HANDLER_ResetCountersCmd_t;
-typedef PLAN_HANDLER_NoArgsCmd_t PLAN_HANDLER_ProcessCmd_t;
+#define PLAN_HANDLER_NOOP_CC                0
+#define PLAN_HANDLER_RESET_COUNTERS_CC      1
+#define PLAN_HANDLER_PROCESS_PLAN_CC        2
 
 /*************************************************************************/
 /*
 ** Type definition (Plan Handler housekeeping)
 */
 
-typedef struct
-{
+typedef struct {
+
     uint8 CommandErrorCounter;
     uint8 CommandCounter;
     uint8 spare[2];
 } PLAN_HANDLER_HkTlm_Payload_t;
 
-typedef struct
-{
+typedef struct {
+
     uint8 TlmHeader[CFE_SB_TLM_HDR_SIZE]; /**< \brief Telemetry header */
     PLAN_HANDLER_HkTlm_Payload_t Payload;         /**< \brief Telemetry payload */
 } OS_PACK PLAN_HANDLER_HkTlm_t;
@@ -74,5 +53,18 @@ typedef union {
     CFE_SB_Msg_t MsgHdr;
     PLAN_HANDLER_HkTlm_t HkTlm;
 } PLAN_HANDLER_HkBuffer_t;
+
+
+/*************************************************************************/
+/*
+** Type definition (Plan Handler - Information pertaining to a plan table)
+*/
+
+typedef struct {
+
+    char filename[64];
+    char locaton[64];
+
+} PLAN_HANDLER_PlanInfo_t;
 
 #endif /* PLAN_HANDLER_MSG_H */
